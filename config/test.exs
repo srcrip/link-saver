41f08d73f2571444b6bfd45c46d8dbd32b1e43cc
Andrew <andrewianstewart@gmail.com>
2025-06-13 16:38:54 -0400

    Run phx.gen.auth

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,8 @@
 import Config
 
+# Only in tests, remove the complexity from the password hashing algorithm
+config :bcrypt_elixir, :log_rounds, 1
+
 # In test we don't send emails
 config :link_saver, LinkSaver.Mailer, adapter: Swoosh.Adapters.Test
 
